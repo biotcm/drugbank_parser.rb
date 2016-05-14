d33180a93f6c1ce711273c8e2717e1fd08ffc0a3
Aidi Stan <aidistan@live.com>
2016-05-14 17:04:32 +0800

    Remove all tabs

--- main.rb
+++ main.rb
@@ -10,38 +10,38 @@ drugs = {}
 reader = XML::Reader.file('drugbank.xml')
 
 while reader.read
-	case reader.node_type
-	when XML::Reader::TYPE_ELEMENT
-		if reader.name == 'drug' && reader.depth == 1
-			drug = {
-				'drugbank-id' => nil,
-				'name' => nil,
-				'groups' => [],
-				'atc-codes' => [],
-				'targets' => []
-			}
-		elsif reader.name == 'atc-code' && reader.depth == 3
-			drug['atc-codes'] << reader.get_attribute('code')
-		end
+  case reader.node_type
+  when XML::Reader::TYPE_ELEMENT
+    if reader.name == 'drug' && reader.depth == 1
+      drug = {
+        'drugbank-id' => nil,
+        'name' => nil,
+        'groups' => [],
+        'atc-codes' => [],
+        'targets' => []
+      }
+    elsif reader.name == 'atc-code' && reader.depth == 3
+      drug['atc-codes'] << reader.get_attribute('code')
+    end
 
-	when XML::Reader::TYPE_TEXT
-		parent = reader.node.parent
+  when XML::Reader::TYPE_TEXT
+    parent = reader.node.parent
 
-		if parent.name == 'drugbank-id' && parent.attributes['primary'] == 'true' && reader.depth == 3
-			drug['drugbank-id'] = reader.value
-		elsif parent.name == 'name' && parent.parent.name == 'drug' && reader.depth == 3
-			drug['name'] = reader.value
-		elsif parent.name == 'group'
-			drug['groups'] << reader.value
-		elsif parent.name == 'gene-name'
-			drug['targets'] << reader.value
-		end
+    if parent.name == 'drugbank-id' && parent.attributes['primary'] == 'true' && reader.depth == 3
+      drug['drugbank-id'] = reader.value
+    elsif parent.name == 'name' && parent.parent.name == 'drug' && reader.depth == 3
+      drug['name'] = reader.value
+    elsif parent.name == 'group'
+      drug['groups'] << reader.value
+    elsif parent.name == 'gene-name'
+      drug['targets'] << reader.value
+    end
 
-	when XML::Reader::TYPE_END_ELEMENT
-		if reader.name == 'drug' && reader.depth == 1
-			drugs[drug['drugbank-id']] = drug
-		end
-	end
+  when XML::Reader::TYPE_END_ELEMENT
+    if reader.name == 'drug' && reader.depth == 1
+      drugs[drug['drugbank-id']] = drug
+    end
+  end
 end
 
 #
@@ -52,15 +52,15 @@ drug2targets = Hash.new { |hash, key| hash[key] = [] }
 target2drugs = Hash.new { |hash, key| hash[key] = [] }
 
 File.open('drugs.txt', 'w') do |fout|
-	fout.puts "DrugBank ID\tName\tGroups\tATC Codes\tTargets"
-	drugs.each_value do |drug|
-		fout.puts drug.values.map { |v| v.is_a?(Array) ? v.join(', ') : v }.join("\t")
+  fout.puts "DrugBank ID\tName\tGroups\tATC Codes\tTargets"
+  drugs.each_value do |drug|
+    fout.puts drug.values.map { |v| v.is_a?(Array) ? v.join(', ') : v }.join("\t")
 
-		drug['targets'].each do |target|
-			drug2targets[drug['name']] << target
-			target2drugs[target] << drug['name']
-		end
-	end
+    drug['targets'].each do |target|
+      drug2targets[drug['name']] << target
+      target2drugs[target] << drug['name']
+    end
+  end
 end
 
 File.open('drug2targets.txt', 'w').puts drug2targets.to_a.map { |a| a.join("\t") }
