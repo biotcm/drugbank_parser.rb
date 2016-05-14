b2b8b439c803aac6d564530701749585052512cc
Aidi Stan <aidistan@live.com>
2016-05-14 16:39:41 +0800

    Rewrite main.rb using libxml-ruby

--- main.rb
+++ main.rb
@@ -1,92 +1,67 @@
 #!/usr/bin/env ruby
-File.open("result.txt","w") do |fout|
-	newdrug=0
-	atc_code_list=[]
-	target_list=[]
-	drugbank_id_temp=""
-	drug_name=""
-	next_line=""
-	drug_group=""
-	a=0
-File.open("drugbank.xml").each_with_index do |fin,index|
-	#if index<=300
-		#line=fin.split("\t",-1)
-		#line.each do |lin|
-			#lin.chomp!
-		#end
-		#fout.puts line.join("\t")
-		fin.chomp!
+require 'xml'
+require 'yaml'
 
-		drugbank_id=/  <drugbank-id primary="true">DB[\d{1}]/.match(fin)
-		if  drugbank_id!=nil#如果drugbank_id不nil,说明进入下一个药物，需输出之前药物atc等信息，否则还在同一个药物中，寻找atc
-			#clear the old drug info
-			if index!=0
-				atc_code_list.each do |atc_each|
-					fout.puts drugbank_id_temp+"\t"+drug_name.to_s+"\t"+atc_each+"\t"+drug_group+"\t"+target_list.join(",")
-				end
-				if atc_code_list==[]
-					fout.puts drugbank_id_temp+"\t"+drug_name.to_s+"\t\t"+drug_group+"\t"+target_list.join(",")
-				end
-				#输出包含无atc编码的药物
-			#fout.puts drugbank_id_temp+"\t"+atc_code_list.join("\t")
-			#一个DB接多个atc code的输出形式
-		    end
-			drugbank_id_temp="DB0"+drugbank_id.post_match[0..3]
-			atc_code_list=[]
-			target_list=[]
-			drug_name=""
-		end
-		#atc编码（一个药物对应多个atc code则分多行写）
-		atc_code=/atc-code code+/.match(fin)
-		if atc_code!=nil
-			atc_code_list.push(fin[20..26])
+#
+# Parse
+#
+
+drugs = {}
+reader = XML::Reader.file('drugbank.xml')
+
+while reader.read
+	case reader.node_type
+	when XML::Reader::TYPE_ELEMENT
+		if reader.name == 'drug' && reader.depth == 1
+			drug = {
+				'drugbank-id' => nil,
+				'name' => nil,
+				'groups' => [],
+				'atc-codes' => [],
+				'targets' => []
+			}
+		elsif reader.name == 'atc-code' && reader.depth == 3
+			drug['atc-codes'] << reader.get_attribute('code')
 		end
 
-		#targets
-		if /  <targets>/.match(fin)!=nil
-			a=1
-			next
-		end
-		if a==1
-		    targets=/    <gene-name>+/.match(fin)
-		    if targets!=nil
-			    target_gene_name=/<\/gene-name>/.match(targets.post_match)
-			    if target_gene_name!=nil
-			        target_list.push(target_gene_name.pre_match)
-		        end
-		    end
-		    if /  <\/targets>/.match(fin)!=nil
-			    a=0
-		    end
-	    end
+	when XML::Reader::TYPE_TEXT
+		parent = reader.node.parent
 
-	    #drug_name
-		if /  <drugbank-id+/.match(fin)!=nil and drug_name==""
-			next_line="drug_name"
-			next
-		end
-		if next_line=="drug_name"
-		   next_line=""
-			drug_name_temp=/  <name>+/.match(fin)
-		    if drug_name_temp!=nil
-			    drug_name_post=/<\/name>/.match(drug_name_temp.post_match)
-			    if drug_name_post!=nil
-				    drug_name=drug_name_post.pre_match
-			    end
-		    end
-
+		if parent.name == 'drugbank-id' && parent.attributes['primary'] == 'true' && reader.depth == 3
+			drug['drugbank-id'] = reader.value
+		elsif parent.name == 'name' && parent.parent.name == 'drug' && reader.depth == 3
+			drug['name'] = reader.value
+		elsif parent.name == 'group'
+			drug['groups'] << reader.value
+		elsif parent.name == 'gene-name'
+			drug['targets'] << reader.value
 		end
 
-		#drug group(批准/实验/...)
-		drug_group_temp=/    <group>+/.match(fin)
-		    if drug_group_temp!=nil
-			    drug_group_post=/<\/group>/.match(drug_group_temp.post_match)
-			    if drug_group_post!=nil
-				    drug_group=drug_group_post.pre_match
-			    end
-		    end
-
-
-	#end
+	when XML::Reader::TYPE_END_ELEMENT
+		if reader.name == 'drug' && reader.depth == 1
+			drugs[drug['drugbank-id']] = drug
+		end
+	end
 end
+
+#
+# Output
+#
+
+drug2targets = Hash.new { |hash, key| hash[key] = [] }
+target2drugs = Hash.new { |hash, key| hash[key] = [] }
+
+File.open('drugs.txt', 'w') do |fout|
+	fout.puts "DrugBank ID\tName\tGroups\tATC Codes\tTargets"
+	drugs.each_value do |drug|
+		fout.puts drug.values.map { |v| v.is_a?(Array) ? v.join(', ') : v }.join("\t")
+
+		drug['targets'].each do |target|
+			drug2targets[drug['name']] << target
+			target2drugs[target] << drug['name']
+		end
+	end
 end
+
+File.open('drug2targets.txt', 'w').puts drug2targets.to_a.map { |a| a.join("\t") }
+File.open('target2drugs.txt', 'w').puts drug2targets.to_a.map { |a| a.join("\t") }
